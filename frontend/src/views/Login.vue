7a2ce1aa6415ad865f8d18deb662b45160299a52
feirc <feirc@qq.com>
2026-07-08 11:33:00 +0800

    自定义login页面

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,50 +1,34 @@
 <template>
+
   <div id="login" :class="{ recaptcha: recaptcha }">
-    <form @submit="submit">
-      <img :src="logoURL" alt="File Browser" />
-      <h1>{{ name }}</h1>
-      <p v-if="reason != null" class="logout-message">
-        {{ t(`login.logout_reasons.${reason}`) }}
-      </p>
-      <div v-if="error !== ''" class="wrong">{{ error }}</div>
+    <f-background>
+      <form @submit="submit">
+        <img :src="logoURL" alt="File Browser" />
+        <h1>{{ name }}</h1>
+        <p v-if="reason != null" class="logout-message">
+          {{ t(`login.logout_reasons.${reason}`) }}
+        </p>
+        <div v-if="error !== ''" class="wrong">{{ error }}</div>
 
-      <input
-        autofocus
-        class="input input--block"
-        type="text"
-        autocapitalize="off"
-        v-model="username"
-        :placeholder="t('login.username')"
-      />
-      <input
-        class="input input--block"
-        type="password"
-        v-model="password"
-        :placeholder="t('login.password')"
-      />
-      <input
-        class="input input--block"
-        v-if="createMode"
-        type="password"
-        v-model="passwordConfirm"
-        :placeholder="t('login.passwordConfirm')"
-      />
+        <input autofocus class="input input--block" type="text" autocapitalize="off" v-model="username"
+          :placeholder="t('login.username')" />
+        <input class="input input--block" type="password" v-model="password" :placeholder="t('login.password')" />
+        <input class="input input--block" v-if="createMode" type="password" v-model="passwordConfirm"
+          :placeholder="t('login.passwordConfirm')" />
 
-      <div v-if="recaptcha" id="recaptcha"></div>
-      <input
-        class="button button--block"
-        type="submit"
-        :value="createMode ? t('login.signup') : t('login.submit')"
-      />
+        <div v-if="recaptcha" id="recaptcha"></div>
+        <input class="button button--block" type="submit" :value="createMode ? t('login.signup') : t('login.submit')" />
 
-      <p @click="toggleMode" v-if="signup">
-        {{ createMode ? t("login.loginInstead") : t("login.createAnAccount") }}
-      </p>
-    </form>
+        <p @click="toggleMode" v-if="signup">
+          {{ createMode ? t("login.loginInstead") : t("login.createAnAccount") }}
+        </p>
+      </form>
+    </f-background>
   </div>
 </template>
 
 <script setup lang="ts">
+import FBackground from "@/components/FBackground/index.vue"
 import { StatusError } from "@/api/utils";
 import * as auth from "@/utils/auth";
 import {
